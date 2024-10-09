471e84370a14b7b1fb1b83410cd33078762ac1fd
Dancun Omondi <70913596+ojumbo-do@users.noreply.github.com>
2024-10-09 12:44:12 +0300

    updated homescreen and board of directors page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
