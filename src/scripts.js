d6cd45afdca874163d314cc3cf73f09c1b639d57
Dancun Omondi <70913596+ojumbo-do@users.noreply.github.com>
2024-10-09 17:39:24 +0300

    updated about us dropdown

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,66 @@
-"strict"
+"strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const communicationCentreDropdown = document.getElementById('communicationCentreDropdown');
-  const communicationDropdown = document.getElementById('communicationDropdown');
+document.addEventListener("DOMContentLoaded", function () {
+  const communicationCentreDropdown = document.getElementById(
+    "communicationCentreDropdown"
+  );
+  const communicationDropdown = document.getElementById(
+    "communicationDropdown"
+  );
 
-  communicationCentreDropdown.addEventListener('click', function (event) {
+  communicationCentreDropdown.addEventListener("click", function (event) {
     event.preventDefault();
-    communicationDropdown.classList.toggle('hidden');
+    communicationDropdown.classList.toggle("hidden");
   });
 
-  document.addEventListener('click', function (event) {
-    if (!communicationCentreDropdown.contains(event.target) && !communicationDropdown.contains(event.target)) {
-      communicationDropdown.classList.add('hidden');
+  document.addEventListener("click", function (event) {
+    if (
+      !communicationCentreDropdown.contains(event.target) &&
+      !communicationDropdown.contains(event.target)
+    ) {
+      communicationDropdown.classList.add("hidden");
     }
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutUsDropdown = document.getElementById("dropdownNavbar");
+  const aboutUs = document.getElementById("dropdownLargeButton");
 
+  aboutUsDropdown.addEventListener("click", function (event) {
+    event.preventDefault();
+    aboutUs.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !aboutUsDropdown.contains(event.target) &&
+      !aboutUs.contains(event.target)
+    ) {
+      aboutUs.classList.add("hidden");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const communicationCentreDropdown = document.getElementById(
+    "communicationCentreDropdown"
+  );
+  const communicationDropdown = document.getElementById(
+    "communicationDropdown"
+  );
+
+  communicationCentreDropdown.addEventListener("click", function (event) {
+    event.preventDefault();
+    communicationDropdown.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !communicationCentreDropdown.contains(event.target) &&
+      !communicationDropdown.contains(event.target)
+    ) {
+      communicationDropdown.classList.add("hidden");
+    }
+  });
+});
